4103c2997d0bde8f13be4109dafc886c5310c4da
carneirofc <claudiofcarneiro@hotmail.com>
2019-06-12 16:16:49 -0300

    Booster cmd: Autosave and env

--- iocBoot/iocSSABooster/st.cmd
+++ iocBoot/iocSSABooster/st.cmd
@@ -13,10 +13,26 @@ SSA_registerRecordDeviceDriver pdbbase
 
 drvAsynIPPortConfigure("L0", "unix://${TOP}/SSATestingServer/socket")
 
-dbLoadRecords("db/devSSABooster.db","P=Test,R=:,PORT=L0,A=0")
+epicsEnvSet("P","RA-ToBO")
+epicsEnvSet("R",":")
+
+dbLoadRecords("db/SSABoosterAutosave.db","P=$(P),R=$(R)")
+dbLoadRecords("db/SSABooster.db","P=$(P),R=$(R),TORRE=TORRE1,PORT=L0,A=0")
+
+# Offsets
+set_pass0_restoreFile("autosave/SSABooster/$(P)$(R)Offsets.sav")
+set_pass1_restoreFile("autosave/SSABooster/$(P)$(R)Offsets.sav")
+
+# Alarms
+set_pass0_restoreFile("autosave/SSABooster/$(P)$(R)Alarms.sav")
+set_pass1_restoreFile("autosave/SSABooster/$(P)$(R)Alarms.sav")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 # Enable debug
 #var streamDebug 1
+
+cd "${TOP}"
+create_monitor_set("$(TOP)/db/SSABoosterAlarms.req",  10, "TOP=$(TOP), SAVENAMEPV=$(P)$(R)AlarmsSaveName")
+create_monitor_set("$(TOP)/db/SSABoosterOffsets.req", 10, "TOP=$(TOP), SAVENAMEPV=$(P)$(R)OffsetsSaveName")
