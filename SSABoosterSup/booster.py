c3e0d4bf7857a0f10026ff464cace6b6df2b7f52
carneirofc <claudiofcarneiro@hotmail.com>
2019-06-11 23:18:23 -0300

    SupBooster: Offsets

--- SSABoosterSup/booster.py
+++ SSABoosterSup/booster.py
@@ -76,6 +76,20 @@ record(calcout, "${PV}_HIGH"){
     field(OUT, "${PV}.HIGH")
 }
 
+record(calcout, "${PV}_LOW"){
+    field(CALC, "A")
+    field(INPA, "${LOW} CP")
+
+    field(OUT, "${PV}.LOW")
+}
+
+record(calcout, "${PV}_LOLO"){
+    field(CALC, "A")
+    field(INPA, "${LOLO} CP")
+
+    field(OUT, "${PV}.LOLO")
+}
+
 record(calc, "${PV}"){
     field(CALC, "(4.8321*A -2.4292) + B")
     field(INPA, "${PV}_v CP MSS")
@@ -96,6 +110,8 @@ record(calc, "${PV}"){
 :param PV:      PV name
 :param HIHI:    Alarm PV name
 :param HIGH:    Alarm PV name
+:param LOW:     Alarm PV name
+:param LOLO:    Alarm PV name
 :param D:       Description text
 :param OFS:     Offset PV name
 :param N:       Reading block number
@@ -122,6 +138,20 @@ record(calcout, "${PV}_HIGH"){
     field(OUT, "${PV}.HIGH")
 }
 
+record(calcout, "${PV}_LOW"){
+    field(CALC, "A")
+    field(INPA, "${LOW} CP")
+
+    field(OUT, "${PV}.LOW")
+}
+
+record(calcout, "${PV}_LOLO"){
+    field(CALC, "A")
+    field(INPA, "${LOLO} CP")
+
+    field(OUT, "${PV}.LOLO")
+}
+
 record(calc, "${PV}"){
     field(CALC, "(10 * LOG((11.4*(A*A) + 1.7*A + 0.01))) + B")
     field(INPA, "${PV}_v CP MSS")
@@ -140,19 +170,19 @@ record(calc, "${PV}"){
 class Data():
     def __init__(self, index, row):
        self.index = index
-       self.Tower = row['Tower'] 
-       self.HeatSink = row['HeatSink'] 
-       self.Reading = row['Reading'] 
-       self.Valor = row['Valor'] 
-       self.Sec = row['Sec'] 
-       self.Sub = row['Sub'] 
-       self.Dis = row['Dis'] 
-       self.Dev = row['Dev'] 
-       self.Idx = row['Idx'] 
-       self.Prop = row['Prop'] 
-       self.Device = row['Device Name'] 
-       self.Indicative = row['Indicative'] 
-       self.Module = row['Module'] 
+       self.Tower = row['Tower']
+       self.HeatSink = row['HeatSink']
+       self.Reading = row['Reading']
+       self.Valor = row['Valor']
+       self.Sec = row['Sec']
+       self.Sub = row['Sub']
+       self.Dis = row['Dis']
+       self.Dev = row['Dev']
+       self.Idx = row['Idx']
+       self.Prop = row['Prop']
+       self.Device = row['Device Name']
+       self.Indicative = row['Indicative']
+       self.Module = row['Module']
 
 
 if __name__ == '__main__':
@@ -163,11 +193,36 @@ if __name__ == '__main__':
 
     for index, row in sheet.iterrows():
        entries.append(Data(index, row))
-    
+
     db = ''
     db += raw_data.safe_substitute()
-    
+
     #@todo: Print OFS and Alarms
+    '''
+    PwrRevBot-Mon
+    PwrFwdBot-Mon
+    PwrRevTop-Mon
+    PwrFwdTop-Mon
+
+    PwrFwdOut-Mon
+    PwrRevOut-Mon
+    PwrFwdIn-Mon
+    PwrRevIn-Mon
+    '''
+    GENERAL_POWER_HIHI  = ':AlarmConfig:GeneralPowerLimHihi'
+    GENERAL_POWER_HIGH  = ':AlarmConfig:GeneralPowerLimHigh'
+    GENERAL_POWER_LOW   = ':AlarmConfig:GeneralPowerLimLow'
+    GENERAL_POWER_LOLO  = ':AlarmConfig:GeneralPowerLimLoLo'
+
+    INNER_POWER_HIHI    = ':AlarmConfig:InnerPowerLimHihi'
+    INNER_POWER_HIGH    = ':AlarmConfig:InnerPowerLimHigh'
+    INNER_POWER_LOW     = ':AlarmConfig:InnerPowerLimLow'
+    INNER_POWER_LOLO    = ':AlarmConfig:InnerPowerLimLolo'
+
+    CURRENT_POWER_HIHI  = ':AlarmConfig:CurrentLimHihi'
+    CURRENT_POWER_HIGH  = ':AlarmConfig:CurrentLimHigh'
+    CURRENT_POWER_LOW   = ':AlarmConfig:CurrentLimLow'
+    CURRENT_POWER_LOLO  = ':AlarmConfig:CurrentLimLolo'
 
     for e in entries:
         if e.Tower != '1':
@@ -181,19 +236,31 @@ if __name__ == '__main__':
         if e.index == 0:
             db += pwr_sts.safe_substitute(**kwargs)
             continue
-        
+
         if e.HeatSink == '9':
             # General Power
+            kwargs['HIHI'] = e.Sec + '-' + e.Sub + GENERAL_POWER_HIHI
+            kwargs['HIGH'] = e.Sec + '-' + e.Sub + GENERAL_POWER_HIGH
+            kwargs['LOW']  = e.Sec + '-' + e.Sub + GENERAL_POWER_LOW
+            kwargs['LOLO'] = e.Sec + '-' + e.Sub + GENERAL_POWER_LOLO
             db += power.safe_substitute(**kwargs)
             continue
 
         if int(e.Reading) < 35:
             # Current
+            kwargs['HIHI'] = e.Sec + '-' + e.Sub + CURRENT_POWER_HIHI
+            kwargs['HIGH'] = e.Sec + '-' + e.Sub + CURRENT_POWER_HIGH
+            kwargs['LOW']  = e.Sec + '-' + e.Sub + CURRENT_POWER_LOW
+            kwargs['LOLO'] = e.Sec + '-' + e.Sub + CURRENT_POWER_LOLO
             db += volt.safe_substitute(**kwargs)
             continue
 
         else:
             # Power
+            kwargs['HIHI'] = e.Sec + '-' + e.Sub + INNER_POWER_HIHI
+            kwargs['HIGH'] = e.Sec + '-' + e.Sub + INNER_POWER_HIGH
+            kwargs['LOW']  = e.Sec + '-' + e.Sub + INNER_POWER_LOW
+            kwargs['LOLO'] = e.Sec + '-' + e.Sub + INNER_POWER_LOLO
             db += power.safe_substitute(**kwargs)
             continue
 
