687d204d5bdbecd127b87bfa1364fcbfbdd46134
carneirofc <claudiofcarneiro@hotmail.com>
2021-04-06 09:08:14 -0300

    Update generral power equations. FwdIn will use Eq1.

--- SSAStorageRingSup/ring.py
+++ SSAStorageRingSup/ring.py
@@ -328,14 +328,14 @@ if __name__ == '__main__':
 
             if int(e.Reading) in [1, 5, 9, 13]:
                 kwargs['OFS'] = e.Sec + '-' + e.Sub + OUTPUT_INCIDENT
-                db += power_general_eq1.safe_substitute(**kwargs)
+                db += power_general_eq2.safe_substitute(**kwargs)
             elif int(e.Reading) in [2, 6, 10, 14]:
                 kwargs['OFS'] = e.Sec + '-' + e.Sub + OUTPUT_REFLECTED
-                db += power_general_eq1.safe_substitute(**kwargs)
+                db += power_general_eq2.safe_substitute(**kwargs)
 
             elif int(e.Reading) in [3, 7, 11, 15]:
                 kwargs['OFS'] = e.Sec + '-' + e.Sub + INPUT_INCIDENT
-                db += power_general_eq2.safe_substitute(**kwargs)
+                db += power_general_eq1.safe_substitute(**kwargs)
             elif int(e.Reading) in [4, 8, 12, 16]:
                 kwargs['OFS'] = e.Sec + '-' + e.Sub + INPUT_REFLECTED
                 db += power_general_eq2.safe_substitute(**kwargs)
