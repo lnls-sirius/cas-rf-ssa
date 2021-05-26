0a607b621e66fe03c9be70893fb645dae8ce343d
carneirofc <claudiofcarneiro@hotmail.com>
2021-05-26 09:20:28 -0300

    Storage ring test cmd

---
+++ iocBoot/iocSSAStorageRing/st-test.cmd
@@ -0,0 +1,26 @@
+#!../../bin/linux-x86_64/SSA
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/SSA.dbd"
+SSA_registerRecordDeviceDriver pdbbase
+
+drvAsynIPPortConfigure("L0", "unix://${TOP}/SSATestingServer/socket")
+
+epicsEnvSet("P","RA-ToSIA01")
+epicsEnvSet("R",":")
+
+dbLoadRecords("db/SSAStorageRing01.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+# Enable debug
+#var streamDebug 1
+
+cd "${TOP}"
+
+seq SSAStorageRing "prefix=$(P)"
