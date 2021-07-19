02d07fd406d764f83d9583040eb904b8cd8c4a57
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-19 11:04:18 -0300

    sort entries

--- SSAStorageRingSup/build-database.sh
+++ SSAStorageRingSup/build-database.sh
@@ -4,8 +4,21 @@ set -exu
 [ -z "$1" ] && echo "arg 1) TOP" && exit 1 || TOP=$1
 
 python ring.py > SSAStorageRing01.db
-cat SSAStorageRing01.db | grep record | grep AlarmConfig | grep -Po  '(?<=")(.*?)(?="\){)' > SSAStorageRing01Alarms.req
-cat SSAStorageRing01.db | grep record | grep OffsetConfig | grep -Po  '(?<=")(.*?)(?="\){)' > SSAStorageRing01Offsets.req
+
+cat SSAStorageRing01.db | \
+    grep record | \
+    grep AlarmConfig | \
+    grep -Po  '(?<=")(.*?)(?="\){)' | \
+    sort -u \
+    > SSAStorageRing01Alarms.req
+
+cat SSAStorageRing01.db | \
+    grep record | \
+    grep OffsetConfig | \
+    grep -Po  '(?<=")(.*?)(?="\){)' | \
+    sort -u  \
+    > SSAStorageRing01Offsets.req
+
 sed -e 's/RA-ToSIA01/RA-ToSIA02/g' SSAStorageRing01.db > SSAStorageRing02.db
 sed -e 's/RA-ToSIA01/RA-ToSIA02/g' SSAStorageRing01Offsets.req > SSAStorageRing02Offsets.req
 sed -e 's/RA-ToSIA01/RA-ToSIA02/g' SSAStorageRing01Alarms.req > SSAStorageRing02Alarms.req
