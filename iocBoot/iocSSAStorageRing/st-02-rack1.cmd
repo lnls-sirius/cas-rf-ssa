61e67d16febde4a89d5d2900aadb05217d520f73
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-19 11:34:37 -0300

    Update st cmd

--- iocBoot/iocSSAStorageRing/st-02-rack1.cmd
+++ iocBoot/iocSSAStorageRing/st-02-rack1.cmd
@@ -11,15 +11,10 @@ SSA_registerRecordDeviceDriver pdbbase
 drvAsynSerialPortConfigure("L0", "/dev/ttyUSB0")
 asynSetOption("L0", 0, "baud", "500000")
 
-epicsEnvSet("P","RA-ToSIA01")
+epicsEnvSet("P","RA-ToSIA02")
 epicsEnvSet("R",":")
 
 dbLoadRecords("db/SIRack1.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
-dbLoadRecords("db/SIRack2.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
-dbLoadRecords("db/SIRack3.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
-dbLoadRecords("db/SIRack4.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
-dbLoadRecords("db/SISettings.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
-dbLoadRecords("db/SITotalDcCurrent.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
 
 #save_restoreSet_FilePermissions(0777)
 
@@ -33,18 +28,13 @@ save_restoreSet_SeqPeriodInSeconds(600)
 
 set_savefile_path("$(TOP)/autosave/SSAStorageRing")
 
-set_pass0_restoreFile("SSAStorageRing01.sav")
-set_pass1_restoreFile("SSAStorageRing01.sav")
+set_pass0_restoreFile("SSAStorageRing02.sav")
+set_pass1_restoreFile("SSAStorageRing02.sav")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-# Enable debug
-#var streamDebug 1
-
 cd "${TOP}"
-create_monitor_set("SSAStorageRing01.req",  10)
+create_monitor_set("SSAStorageRing02.req",  10)
 
 dbl
-
-seq &SSAStorageRingCurrentCalc "prefix=$(P)"
