9a7ea0370d85653cf0b4841bf4c0d74dd57f85a8
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-22 08:53:19 -0300

    ssa si: rack1

--- iocBoot/iocSSAStorageRing/st-02.cmd
+++ iocBoot/iocSSAStorageRing/st-02.cmd
@@ -14,6 +14,7 @@ asynSetOption("L0", 0, "baud", "500000")
 epicsEnvSet("P","RA-ToSIA02")
 epicsEnvSet("R",":")
 
+dbLoadRecords("db/SIRack1.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
 dbLoadRecords("db/SIRack2.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
 dbLoadRecords("db/SIRack3.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
 dbLoadRecords("db/SIRack4.db", "P=$(P),R=$(R),RACK=RACK,PORT=L0,A=0,SCAN=2")
